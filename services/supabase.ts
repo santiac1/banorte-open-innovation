39ca11d78a9a894a4d138c851b08aaf8ee916af4
ManuelPerezF <A01741957@tec.mx>
2025-10-24 22:47:43 -0600

    Agregar archivo supabase.ts con configuración del cliente Supabase

--- services/supabase.ts
+++ services/supabase.ts
@@ -4,4 +4,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ipzitalmkqqvowfafpbh.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey!) 
